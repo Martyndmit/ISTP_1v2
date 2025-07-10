092bbd09114de5c9e145bbdb07c4ca717455af98
Martyndmit <165693715+Martyndmit@users.noreply.github.com>
2025-07-10 12:31:13 +0300

    Delete src/GymManagement directory

--- src/GymManagement/GymInfrastructure/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace GymManagementMVC.Models
-{
-    public class User : IdentityUser
-    {
-        public int Year { get; set; } // рік народження або інші властивості
-    }
-}
